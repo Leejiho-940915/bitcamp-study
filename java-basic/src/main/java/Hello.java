6f32cdb3c7808aa58f0f24bdf302dc6505033169
Leejiho-940915 <wlgh4585@daum.net>
2021-06-30 17:39:51 +0900

    .

---
+++ java-basic/src/main/java/Hello.java
@@ -0,0 +1,8 @@
+class Hello{
+ // Javac로 class 파일을 생성후 Java로 실행해야된다.
+ //  <<주석 (프로그램에 영향을 받지 않고 설명 할 수있다.)
+    public static void main(String[] args){
+    System.out.println("Hello, world!");
+    System.out.println("안녕하세요!");
+    }
+}
