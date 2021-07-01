3739a6830966f75e9e203280deb13d7f6384be96
Leejiho-940915 <wlgh4585@daum.net>
2021-07-01 17:56:08 +0900

    .

--- java-basic/src/main/java/com/eomcs/lang/ex03/Exam0210.java
+++ java-basic/src/main/java/com/eomcs/lang/ex03/Exam0210.java
@@ -1,1 +1,10 @@
 package com.eomcs.lang.ex03;
+pubilc static void main(String[]args){
+    //10진수 리터럴
+    //- 코드에서 일반적으로 정수 값을 표현할 때 사용한다.
+    System.out.println(100);
+
+    //8진수 리터럴
+    //- 코드를 작성할 때 잘 사용하지 않는다.
+    //- 0으로 시작해야 한다.
+}
