7bda5e4065726a9d1be086b12b1474c422015bfe
Leejiho-940915 <wlgh4585@daum.net>
2021-07-02 18:44:11 +0900

    5

--- java-basic/src/main/java/com/eomcs/lang/Hello3.java
+++ java-basic/src/main/java/com/eomcs/lang/Hello3.java
@@ -53,6 +53,8 @@ package com.eomcs.lang;
 class Hello3 {
   public static void main(String[] args) {
     System.out.println("Hello, World!");
+    System.out.println("Hello");
+    System.out.println("okok");
   }
 }
 
