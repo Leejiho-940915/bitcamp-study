3e5f802e24634f83abb8cc7e6f3af6e4886567c6
Leejiho-940915 <wlgh4585@daum.net>
2021-07-02 18:26:07 +0900

    5

--- java-basic/javadoc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"com.eomcs.lang.ex02","c":"Exam0200","l":"Exam0200()","url":"%3Cinit%3E()"},{"p":"com.eomcs.lang.ex02","c":"Exam0200","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"com.eomcs.lang.ex02","c":"Exam0200","l":"message"}]
--- java-basic/src/main/java/com/eomcs/lang/ex03/Exam0330.java
+++ java-basic/src/main/java/com/eomcs/lang/ex03/Exam0330.java
@@ -1,10 +1,16 @@
 package com.eomcs.lang.ex03;
-public class Exam0330{
-    public static void main(String[]args){
-        System.out.println(Float.MAX_VALUE); //3.4028235E38
-        System.out.println(Float.MIN_VALUE); //1.4E-45
 
-        System.out.println(Double.MAX_VALUE); //1.7976931348623157E308
-        System.out.println(Double.MIN_VALUE); //4.9E-324
-    }
+//# 부동소수점 리터럴 - 최소값과 최대값
+
+public class Exam0330 {
+  public static void main(String[] args) {
+    
+    //## 4바이트 부동소수점의 최대값과 최소값
+    System.out.println(Float.MAX_VALUE);
+    System.out.println(Float.MIN_VALUE);
+    
+    //## 8바이트 부동소수점의 최대값과 최소값
+    System.out.println(Double.MAX_VALUE);
+    System.out.println(Double.MIN_VALUE);
+  }
 }
