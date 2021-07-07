05d89f8063fdc0489008e89e698c2adf8210b535
Leejiho-940915 <wlgh4585@daum.net>
2021-07-07 20:53:59 +0900

    0707

---
+++ java-basic/src/main/java/com/eomcs/lang/ed01/Exam5.java
@@ -0,0 +1,10 @@
+package com.eomcs.lang.ed01;
+
+public class Exam5{
+  public static void main(String[]args){
+    int age;
+    age=20;
+    System.out.print(age+20);
+
+  }
+}
--- study-project/app/src/main/java/com/eomcs/pms/App.java
+++ study-project/app/src/main/java/com/eomcs/pms/App.java
@@ -2,14 +2,46 @@ package com.eomcs.pms;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("[회원]");
-    System.out.println("번호: 101");
-    System.out.println("이름: 홍길동");
-    System.out.println("이메일: hong@test.com");
-    System.out.println("암호: 1111");
-    System.out.println("사진: hong.jpg");
-    System.out.println("전화: 010-1111-2222");
-    System.out.println("가입일: 2021-07-05");    
+
+    java.util.Scanner keyboardScanner = new java.util.Scanner(System.in);
+
+    System.out.print("번호? ");
+    int number = keyboardScanner.nextInt();
+    keyboardScanner.nextLine();
+
+    System.out.print("이름? ");
+    String name = keyboardScanner.next();
+
+    System.out.print("이메일? ");
+    String email = keyboardScanner.nextLine();
+
+    System.out.print("암호? ");
+    int password = keyboardScanner.nextInt();
+    keyboardScanner.nextLine();
+
+    System.out.print("사진? ");
+    String picture = keyboardScanner.next();
+    keyboardScanner.nextLine();
+
+    System.out.print("전화? ");
+    int tel = keyboardScanner.nextInt();
+    keyboardScanner.nextLine();
+
+
+    java.sql.Date registeredDate = new java.sql.Date(System.currentTimeMillis());
+
+    keyboardScanner.close();
+
+    System.out.println("-----------------------------------");
+    System.out.printf("번호: %d\n", number);
+    System.out.printf("이름: %s\n", name);
+    System.out.printf("이메일: %s\n", email);
+    System.out.printf("암호: %s\n", password);
+    System.out.printf("사진: %s\n", picture);
+    System.out.printf("전화: %s\n", tel);
+    System.out.printf("가입일: %s\n", registeredDate);
+
+
   }
 }
 
