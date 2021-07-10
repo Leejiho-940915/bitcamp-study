cf39a6110ce5b4bf5cbcbd36b882db4aad450b77
Leejiho-940915 <wlgh4585@daum.net>
2021-07-10 22:10:49 +0900

    0710

---
+++ java-basic/src/test/java/Backjun2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Backjun2 {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int  A,B;
+    A = 1;
+    B = A + 1;
+
+    System.out.print(A+B);
+  }
+
+}
+
+
