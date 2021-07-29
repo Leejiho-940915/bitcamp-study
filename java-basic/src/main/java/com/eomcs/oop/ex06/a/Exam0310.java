471d4ba2142dd0485d636ec43fe074bdeea0944c
Leejiho-940915 <wlgh4585@daum.net>
2021-07-29 15:53:13 +0900

    0729

--- java-basic/src/main/java/com/eomcs/oop/ex06/a/Exam0310.java
+++ java-basic/src/main/java/com/eomcs/oop/ex06/a/Exam0310.java
@@ -13,8 +13,9 @@ public class Exam0310 {
     c.cc = 890;      // Car의 인스턴스 변수
     c.valve = 16;    // Car의 인스턴스 변수
 
-    // 형변환(type casting)으로 컴파일러를 속일 수는 있지만,
-    // 실행할 때 오류가 발생할 것이다.
+    // 잘못된 형변환을 할 경우,
+    // => 형변환(type casting)으로 컴파일러를 속일 수는 있지만,
+    //    실행할 때 오류가 발생할 것이다.
     // => 속이지 말라!
     Sedan s = (Sedan) c; // 실행할 때 오류 발생! (runtime exception)
     s.sunroof = true;
