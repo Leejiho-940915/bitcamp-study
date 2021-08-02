05e768bd1612c7f4ab4a5ee3813c2b4b0195fb9d
Leejiho-940915 <wlgh4585@daum.net>
2021-08-02 21:38:58 +0900

    0802

--- study-project/app/src/main/java/com/eomcs/pms/App.java
+++ study-project/app/src/main/java/com/eomcs/pms/App.java
@@ -6,119 +6,162 @@ import com.eomcs.pms.handler.ProjectHandler;
 import com.eomcs.pms.handler.TaskHandler;
 import com.eomcs.util.Prompt;
 
+// 1) 메인 메뉴를 출력하고 번호를 입력 받는다.(App.java.01)
+//    - 0 번을 입력하면 프로그램을 종료한다.
+// 2) 게시판 메뉴를 출력하고 번호를 입력 받는다.
+//    - 사용자가 입력한 메뉴 번호에 따라 실행할 명령어를 설정한다.
+// 3) 회원/프로젝트/작업 메뉴를 출력하고 번호를 입력 받는다.
+//    - 사용자가 입력한 메뉴 번호에 따라 실행할 명령어를 설정한다.
+// 4) 메뉴 번호를 입력했을 때 해당 기능을 바로 실행하게 한다.
+// 5) 각각의 메인 메뉴를 다루는 코드를 별도의 메서드로 추출한다.
+//    - doBoardMenu() 메서드 정의
+//    - doMemberMenu() 메서드 정의
+//    - doProjectMenu() 메서드 정의
+//    - doTaskMenu() 메서드 정의
+//    - doMainMenu() 메서드 정의
+// 
 public class App {
 
+  // main() 메서드와 doXxxMenu() 메서드가 공유하는 변수는 
+  // 같은 스태틱 멤버로 만든다.
+  static BoardHandler boardHandler = new BoardHandler();
+  static MemberHandler memberHandler = new MemberHandler();
+  static ProjectHandler projectHandler = new ProjectHandler(memberHandler);
+  static TaskHandler taskHandler = new TaskHandler(memberHandler);
+
   public static void main(String[] args) {
-
-    BoardHandler boardHandler = new BoardHandler();
-    MemberHandler memberHandler = new MemberHandler();
-
-
-    // 만약 ProjectHandler의 메서드가 사용할 의존 객체를 주입하지 않는다면?
-    // - 그 의존 객체를 사용하는 메서드를 호출할 때 실행 오류가 발생할 것이다.
-    //    ProjectHandler projectHandler = new ProjectHandler();
-    //    projectHandler.memberHandler = memberHandler;
-    //
-    // 왜 이런 문제가 발생하는가?
-    // - 의존 객체 주입을 강제하지 않기 때문이다.
-    // 해결책?
-    // - 의존 객체 주입을 강제하면 된다.
-    // - ProjectHandler의 인스턴스를 생성할 때 반드시 MemberHandler의 인스턴스를 주입하게 
-    //   만들면 된다.
-    // 어떻게?
-    // - 생성자를 도입하라!
-    ProjectHandler projectHandler = new ProjectHandler(memberHandler);
-
-    // 생성자를 이용하면 다음과 같이 인스턴스를 생성할 때 의존 객체 주입을 강제할 수 있다.
-    TaskHandler taskHandler = new TaskHandler(memberHandler);
-
     while (true) {
-      String input = Prompt.inputString("명령> ");
+      int menuNo = doMainMenu();
 
-      if (input.equals("exit") || input.equals("quit")) {
-        System.out.println("안녕!");
+      if (menuNo == 0) {
         break;
-      } else if (input.equals("/member/add")) {
-        memberHandler.add();
-
-      } else if (input.equals("/member/list")) {
-        memberHandler.list();
-
-      } else if (input.equals("/member/detail")) {
-        memberHandler.detail();
-
-      } else if (input.equals("/member/update")) {
-        memberHandler.update();
-
-      } else if (input.equals("/member/delete")) {
-        memberHandler.delete();
-
-      }  else if (input.equals("/project/add")) {
-        // add() 메서드가 사용할 의존 객체를 미리 주입했기 때문에
-        // 이제 파라미터로 전달할 필요가 없다.
-        //        projectHandler.add(memberHandler); // 이전 코드 
-        projectHandler.add();
-
-      }  else if (input.equals("/project/list")) {
-        projectHandler.list();
-
-      }  else if (input.equals("/project/detail")) {
-        projectHandler.detail();
-
-      }  else if (input.equals("/project/update")) {
-        // update() 메서드가 사용할 의존 객체를 미리 주입했기 때문에
-        // 이제 파라미터로 전달할 필요가 없다.
-        //        projectHandler.update(memberHandler); // 이전 코드
-        projectHandler.update();
-
-      }  else if (input.equals("/project/delete")) {
-        projectHandler.delete();
-
-      }  else if (input.equals("/task/add")) {
-        // add() 메서드가 사용할 의존 객체를 미리 주입했기 때문에
-        // 이제 파라미터로 전달할 필요가 없다.
-        //        taskHandler.add(memberHandler); // 이전 코드
-        taskHandler.add();
-
-      }  else if (input.equals("/task/list")) {
-        taskHandler.list();
-
-      }  else if (input.equals("/task/detail")) {
-        taskHandler.detail();
-
-      }  else if (input.equals("/task/update")) {
-        // update() 메서드가 사용할 의존 객체를 미리 주입했기 때문에
-        // 이제 파라미터로 전달할 필요가 없다.
-        //        taskHandler.update(memberHandler); // 이전 코드
-        taskHandler.update();
-
-      }  else if (input.equals("/task/delete")) {
-        taskHandler.delete();
-
-      }  else if (input.equals("/board/add")) {
-        boardHandler.add();
-
-      }  else if (input.equals("/board/list")) {
-        boardHandler.list();
-
-      }  else if (input.equals("/board/detail")) {
-        boardHandler.detail();
-
-      }  else if (input.equals("/board/update")) {
-        boardHandler.update();
-
-      }  else if (input.equals("/board/delete")) {
-        boardHandler.delete();
-
+      } else if (menuNo == 1) {
+        doBoardMenu();
+      } else if (menuNo == 2) {
+        doMemberMenu();
+      } else if (menuNo == 3) {
+        doProjectMenu();
+      } else if (menuNo == 4) {
+        doTaskMenu();
       } else {
-        System.out.println("실행할 수 없는 명령입니다.");
+        System.out.println("메뉴 번호가 유효하지 않습니다.");
       }
       System.out.println();
     }
 
-    // Prompt 가 소유하고 관리하고 있는 자원을 닫으라고 명령한다. 
     Prompt.close();
   }
+
+  static void doBoardMenu() {
+    while (true) {
+      System.out.println("[메인/게시판]");
+      System.out.println("1. 등록");
+      System.out.println("2. 목록");
+      System.out.println("3. 상세보기");
+      System.out.println("4. 변경");
+      System.out.println("5. 삭제");
+      System.out.println("0. 이전메뉴");
+
+      int menuNo = Prompt.inputInt("게시판> ");
+      switch (menuNo) {
+        case 1: boardHandler.add(); break;
+        case 2: boardHandler.list(); break;
+        case 3: boardHandler.detail(); break;
+        case 4: boardHandler.update(); break;
+        case 5: boardHandler.delete(); break;
+        case 0: return;
+        default:
+          System.out.println("무효한 메뉴 번호입니다.");
+      }
+      System.out.println();
+    }
+  }
+
+  static void doMemberMenu() {
+    while (true) {
+      System.out.println("[메인/회원]");
+      System.out.println("1. 등록");
+      System.out.println("2. 목록");
+      System.out.println("3. 상세보기");
+      System.out.println("4. 변경");
+      System.out.println("5. 삭제");
+      System.out.println("0. 이전메뉴");
+
+      int menuNo = Prompt.inputInt("회원> ");
+      switch (menuNo) {
+        case 1: memberHandler.add(); break;
+        case 2: memberHandler.list(); break;
+        case 3: memberHandler.detail(); break;
+        case 4: memberHandler.update(); break;
+        case 5: memberHandler.delete(); break;
+        case 0: return;
+        default:
+          System.out.println("무효한 메뉴 번호입니다.");
+      }
+      System.out.println();
+    } 
+  }
+
+  static void doProjectMenu() {
+    while (true) {
+      System.out.println("[메인/프로젝트]");
+      System.out.println("1. 등록");
+      System.out.println("2. 목록");
+      System.out.println("3. 상세보기");
+      System.out.println("4. 변경");
+      System.out.println("5. 삭제");
+      System.out.println("0. 이전메뉴");
+
+      int menuNo = Prompt.inputInt("프로젝트> ");
+      switch (menuNo) {
+        case 1: projectHandler.add(); break;
+        case 2: projectHandler.list(); break;
+        case 3: projectHandler.detail(); break;
+        case 4: projectHandler.update(); break;
+        case 5: projectHandler.delete(); break;
+        case 0: return;
+        default:
+          System.out.println("무효한 메뉴 번호입니다.");
+      }
+      System.out.println();
+    }
+  }
+
+  static void doTaskMenu() {
+    while (true) {
+      System.out.println("[메인/작업]");
+      System.out.println("1. 등록");
+      System.out.println("2. 목록");
+      System.out.println("3. 상세보기");
+      System.out.println("4. 변경");
+      System.out.println("5. 삭제");
+      System.out.println("0. 이전메뉴");
+
+      int menuNo = Prompt.inputInt("작업> ");
+      switch (menuNo) {
+        case 1: taskHandler.add(); break;
+        case 2: taskHandler.list(); break;
+        case 3: taskHandler.detail(); break;
+        case 4: taskHandler.update(); break;
+        case 5: taskHandler.delete(); break;
+        case 0: return;
+        default:
+          System.out.println("무효한 메뉴 번호입니다.");
+      }
+      System.out.println();
+    }
+  }
+
+  static int doMainMenu() {
+    System.out.println("[메인]");
+    System.out.println("1. 게시판");
+    System.out.println("2. 회원");
+    System.out.println("3. 프로젝트");
+    System.out.println("4. 작업");
+    System.out.println("0. 종료");
+    return Prompt.inputInt("메인> "); 
+  }
+
 }
 
 
