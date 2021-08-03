982e13158a2e7dded03a670ae2e00c6e534ca28e
Leejiho-940915 <wlgh4585@daum.net>
2021-08-03 10:30:42 +0900

    0803

---
+++ study-project/app/src/main/java/com/eomcs/pms/menu/MenuTest.java
@@ -0,0 +1,11 @@
+package com.eomcs.pms.menu;
+
+public class MenuTest {
+
+  public static void main(String[] args) {
+    MenuGroup rootMenu = new MenuGroup("메인");
+    rootMenu.execute();
+
+  }
+
+}
