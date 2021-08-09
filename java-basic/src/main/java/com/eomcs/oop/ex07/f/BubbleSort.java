12879eaa979969a6513b8d32568711620e9aaf97
Leejiho-940915 <wlgh4585@daum.net>
2021-08-09 15:31:07 +0900

    0809

--- java-basic/src/main/java/com/eomcs/oop/ex07/f/BubbleSort.java
+++ java-basic/src/main/java/com/eomcs/oop/ex07/f/BubbleSort.java
@@ -1,8 +1,11 @@
-package com.eomcs.oop.ex07.b;
+package com.eomcs.oop.ex07.f;
 
-public class BubbleSort {
+public class BubbleSort implements Sorter {
 
-  public void run(int[] values) {
+  // 인터페이스의 추상 메서드를 구현하는 것도 
+  // "오버라이딩" 이라 부른다.
+  @Override
+  public void sort(int[] values) {
     int size = values.length;
     for (int i = 0; i < size - 1; i++) {
       for (int j = 0; j < size - i - 1; j++) {
