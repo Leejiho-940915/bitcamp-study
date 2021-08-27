bc8b7a260ce5142965af98a70e8d92b4e27503f4
Leejiho-940915 <wlgh4585@daum.net>
2021-08-27 09:33:28 +0900

    test

---
+++ study-project/app/src/test/java/Board1.java
@@ -0,0 +1,12 @@
+public class Board1 {
+  private String title;
+  private int type;
+  private String tel;
+  private String email;
+  private String season;
+  private String time;
+  private int viewCount;
+  private String content;
+  private String photo;
+
+}
