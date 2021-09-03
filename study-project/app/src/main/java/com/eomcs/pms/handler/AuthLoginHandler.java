50c3eef3439c59490bf5d3ec650753fcfeb384a5
Leejiho-940915 <wlgh4585@daum.net>
2021-09-03 11:24:06 +0900

    09/03

--- study-project/app/src/main/java/com/eomcs/pms/handler/AuthLoginHandler.java
+++ study-project/app/src/main/java/com/eomcs/pms/handler/AuthLoginHandler.java
@@ -1,6 +1,7 @@
 package com.eomcs.pms.handler;
 
 import java.util.List;
+import com.eomcs.menu.Menu;
 import com.eomcs.pms.domain.Member;
 import com.eomcs.util.Prompt;
 
@@ -9,19 +10,35 @@ public class AuthLoginHandler implements Command {
   List<Member> memberList;
 
   static Member loginUser;
+  static int userAccessLevel = Menu.ACCESS_LOGOUT; // 기본은 로그아웃 된 상태이다.
+
   public static Member getLoginUser() {
     return loginUser;
+  }
+
+  public static int getUserAccessLevel() {
+    return userAccessLevel;
   }
 
   public AuthLoginHandler(List<Member> memberList) {
     this.memberList = memberList;
   }
 
+  @Override
   public void execute() {
     System.out.println("[로그인]");
 
     String email = Prompt.inputString("이메일? ");
     String password = Prompt.inputString("암호? ");
+
+    if (email.equals("root") && password.equals("0000")) {
+      Member root = new Member();
+      root.setName("관리자");
+      root.setEmail("admin@test.com");
+      loginUser = root;
+      userAccessLevel = Menu.ACCESS_ADMIN | Menu.ACCESS_GENERAL;
+      return;
+    } 
 
     Member member = findByEmailPassword(email, password);
 
@@ -30,6 +47,7 @@ public class AuthLoginHandler implements Command {
     } else {
       System.out.printf("%s님 환영합니다!\n", member.getName());
       loginUser = member;
+      userAccessLevel = Menu.ACCESS_GENERAL;
     }
   }
 
