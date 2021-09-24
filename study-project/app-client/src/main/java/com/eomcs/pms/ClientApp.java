34e8c16e727fe62c28692a1cf587ac461be78000
Leejiho-940915 <wlgh4585@daum.net>
2021-09-24 17:16:41 +0900

    21/09/24

---
+++ study-project/app-client/src/main/java/com/eomcs/pms/ClientApp.java
@@ -0,0 +1,20 @@
+package com.eomcs.pms;
+
+public class ClientApp {
+
+  public static void main(String[] args) {
+    System.out.println("[PMS 클라이언트]");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ study-project/app-server/src/main/java/com/eomcs/pms/ServerApp.java
@@ -0,0 +1,20 @@
+package com.eomcs.pms;
+
+public class ServerApp {
+
+  public static void main(String[] args) {
+    System.out.println("[PMS 서버]");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
